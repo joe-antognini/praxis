8a640f93a385a073bc91633ab963ffb6851cb783
J. Antognini <antognini@astronomy.ohio-state.edu>
2014-05-29 13:04:01 -0400

    Cleaned up pi.py
    Added a README file to pi.py.  The core functionality of pi.py was put into
    a function which can now be imported into other modules.  Additionally pi.py
    can now accept command line arguments.

--- pi/pi.py
+++ pi/pi.py
@@ -3,16 +3,14 @@
 #
 # pi
 #
-# Calculate the 1000th digit of pi using the Chudnovsky formula
+# Calculate the digits of pi using the Chudnovsky formula
 #
+
 
 from math import *
 from decimal import *
 
-getcontext().prec = 1050
-precision = Decimal('1e-1030')
-
-def not_converged(x, x_prev):
+def not_converged(x, x_prev, precision):
   '''Return True if the sum has not converged to sufficient precision.'''
   if x == Decimal('0'):
     return True
@@ -21,17 +19,37 @@ def not_converged(x, x_prev):
   else:
     return False
 
-sum = Decimal('0')
-sum_prev = sum
-k = 0
-while not_converged(sum, sum_prev):
-  sum_prev = sum
-  numer = factorial(6 * k) * (13591409 + 545140134 * k)
-  denom = factorial(3 * k) * factorial(k)**3 * (-640320)**(3 * k)
-  frac = Decimal(str(numer)) / Decimal(str(denom))
-  frac *= 12 / (Decimal('640320')**3).sqrt()
-  sum += frac
-  k += 1
+def calc_pi(n):
+  '''Calculate pi to n decimal places.'''
 
-pi_string = str(1 / sum)
-print 'The 1000th digit of pi after the decimal is:', pi_string[1001]
+  # Set the numerical precision used
+  getcontext().prec = n + 50
+  precision = Decimal('1e-' + str(n + 25))
+
+  sum = Decimal('0')
+  sum_prev = sum
+  k = 0
+  while not_converged(sum, sum_prev, precision):
+    sum_prev = sum
+    numer = factorial(6 * k) * (13591409 + 545140134 * k)
+    denom = factorial(3 * k) * factorial(k)**3 * (-640320)**(3 * k)
+    frac = Decimal(str(numer)) / Decimal(str(denom))
+    frac *= 12 / (Decimal('640320')**3).sqrt()
+    sum += frac
+    k += 1
+
+  pi_string = str(1 / sum)
+  return pi_string
+
+if __name__ == '__main__':
+  import sys
+  if len(sys.argv) == 1:
+    print 'The 1000th digit of pi after the decimal is:', calc_pi(1000)[1001]
+  elif len(sys.argv) == 2:
+    try:
+      n = int(sys.argv[1])
+      print calc_pi(n)[:n+2]
+    except ValueError:
+      print >> sys.stderr, "pi.py: argument must be an integer"
+  else:
+    print "usage: pi.py [number of digits]"
