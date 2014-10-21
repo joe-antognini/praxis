3ae59e53aba58b6740b91f35c9b6005d0602562d
J. Antognini <antognini@astronomy.ohio-state.edu>
2014-10-21 12:14:26 -0400

    Fixed coin_count and wrote coin_list functions
    The coin_count function now works correctly.  I wrote a draft of coin_list,
    although it is buggy.  It does not correctly group coins in sub-lists.

--- 442/coinchange.py
+++ 442/coinchange.py
@@ -1,28 +1,72 @@
 #! /usr/bin/env python
 
-def coin_count(n):
+def coin_count(n, coins):
   '''Calculate the number of ways to produce n cents given an unlimited
   number of pennies, nickels, dimes, and quarters.
 
   Input:
     n: int
       The target number of cents
+    coins: list
+      The kinds of coins available
 
   Output:
     int
       The number of ways to reach the target number of cents
   '''
 
-  count = 0
-  coins = (1, 5, 10, 25)
-  for coin in coins:
-    if coin > n:
-      continue
-    elif n == coin:
-      count += 1
+  if len(coins) == 0:
+    return 0
+
+  coin = coins.pop()
+  if coin > n:
+    return coin_count(n, coins[:])
+  elif coin == n:
+    return 1 + coin_count(n, coins[:])
+  else:
+    result1 = coin_count(n, coins[:])
+    coins.append(coin)
+    result2 = coin_count(n-coin, coins)
+    return result1 + result2
+
+def coin_list(n, coins):
+  '''Calculate the number of combinations of coins that will produce n
+  cents.
+
+  Input:
+    n: int
+      The target number of cents
+    coins: list
+      The kinds of coins available
+
+  Output:
+    list
+      A list of lists of combinations of coins that produce n cents.
+  '''
+
+  coin = coins.pop()
+  if coin > n:
+    if coins:
+      return coin_list(n, coins[:])
     else:
-      count += coin_count(n - coin)
-  return count
+      return []
+  elif coin == n:
+    if coins:
+      result = coin_list(n, coins[:])
+      result.append([n])
+      return result
+    else:
+      return [[n]]
+  else:
+    if coins:
+      result1 = coin_list(n, coins[:])
+    else:
+      result1 = []
+    coins.append(coin)
+    result2 = coin_list(n-coin, coins)
+    result2[0].append(coin)
+    return result1 + result2
 
 if __name__ == '__main__':
-  print coin_count(6)
+  print coin_count(40, [1, 5, 10, 25])
+  print coin_list(40, [1, 5, 10, 25])
